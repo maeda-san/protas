eb37585fe8c47a178f8f53d446bae93654c16ece
maeda-san <maeda@snowrobin.jp>
2016-02-01 20:28:05 +0900

    部署一覧ビュー作成

---
+++ resources/views/department/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>部署一覧</title>
+
+<table>
+  <tr>
+    <th>部署名</th>
+    <th>作成日</th>
+  </tr>
+  @foreach($departments as $department)
+    <tr>
+      <td>{{$department->name}}</td>
+      <td>{{$department->created_at}}</td>
+    </tr>
+    @endforeach
+</table>
