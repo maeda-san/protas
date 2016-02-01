148d2da47944da52494c1abffcbb6c4fd0c69c86
maeda-san <maeda@snowrobin.jp>
2016-02-01 20:27:05 +0900

    部署ダミーデータ作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Department;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        // $this->call(UserTableSeeder::class)
+         $this->call(DepartmentTableSeeder::class);
 
         Model::reguard();
+    }
+}
+
+class DepartmentTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('departments')->delete();
+
+        $faker = Faker::create('ja_JP');
+
+        for ($i = 0; $i < 100; $i++) {
+            Department::create([
+                'name' => $faker->word
+            ]);
+        }
     }
 }
